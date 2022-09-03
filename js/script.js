964deb58e5a72d215c8abebad58d6d3339dbb9ef
mushfik16381 <ahmed.mushfik16381@gmail.com>
2022-09-03 20:50:34 +0600

    catagory details update

--- js/script.js
+++ js/script.js
@@ -8,28 +8,64 @@ const displayCatagory = (catagories) =>{
     catagories.forEach(catagory => {
         const catagoryDiv = document.createElement('li');
 
-        catagoryDiv.classList.add('country');
         catagoryDiv.innerHTML=`
-            <li>${catagory.category_name}</li>
+        <a href="#" onclick="catagoryLoadDeatails('${catagory.category_id}')">${catagory.category_name}</a>
         `;
         catagoryContainer.appendChild(catagoryDiv);
-        console.log(catagory)
+        // console.log(catagory)
     });
 }
 
-const catagoryDeatails = () => {
-    const url = ` https://openapi.programming-hero.com/api/news/category/01`
+const catagoryLoadDeatails = (category_id) => {
+    const url = ` https://openapi.programming-hero.com/api/news/category/${category_id}`
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayCatagoryDetails(data.data))
 }
 
-const displayCatagoryDetails = () =>{
-    const DetailsContainer = document.getElementById('row');
-    detailsDiv
+const displayCatagoryDetails = (catagoryDetails) =>{
+    const detailsContainer = document.getElementById('detail');
+    detailsContainer.innerHTML = '';
+    catagoryDetails.forEach(catagoryDetail => {
+        const catagoryDetailsDiv = document.createElement('div');
+        catagoryDetailsDiv.classList.add('data')
+        catagoryDetailsDiv.innerHTML =`
+        <div class="row">
+                  <div class="col-md-3">
+                    <img src="${catagoryDetail.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+                </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                        <h5 class="card-title pb-3">${catagoryDetail.title}</h5>
+                        <p class="card-text">${catagoryDetail.details}</p>
+                        <div class="d-flex author justify-content-between align-items-center mt-3">
+                        <div class="profile-img d-flex align-items-center">
+                         
+                                <img class="img-fluid profile-img" src="${catagoryDetail.author.img}" alt="">
+                      
+                            <div class="text ">
+                                <p style="color: #FF6F3F;">${catagoryDetail.author.name ? catagoryDetail.author.name: 'no author found'}</p>
+                                <p>${catagoryDetail.author.published_date ? catagoryDetail.author.published_date: 'no date found'}</p>
+                            </div>
+                        </div>
+                        <div><p>${catagoryDetail.total_view ? catagoryDetail.total_view: 'no view'}</p></div>
+                        <div>
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        Read More
+                        </button>
+                        </div>
+                        </div>
+
+                    </div>
+                </div>
+
+        `;
+        detailsContainer.appendChild(catagoryDetailsDiv);
+      
+    });
 }
 
 
 
-catagoryDeatails()
+catagoryLoadDeatails()
 loadCatagory()
